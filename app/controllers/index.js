95acb81416c943000591627711bab40bd29b599c
DR <d.retnan@nhubnigeria.com>
2016-12-30 23:13:54 +0000

    EJS powered views

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,6 +1,34 @@
 module.exports = function (app) {
 
 	app.get('/',function (req, res, next) {
-		res.render('index', {title: "This is the Logical Address Server"});
+		res.render('pages/index', {
+			title: "Logical Address | Welcome to Logical Address",
+			page: 'home',
+			app_title: "Logical Address"
+		});
+	});
+	
+	app.get('/login',function (req, res, next) {
+		res.render('pages/login', {
+			title: "Logical Address | Login to Logical Address",
+			page: 'login',
+			app_title: "Logical Address"
+		});
+	});
+	
+	app.get('/register',function (req, res, next) {
+		res.render('pages/register', {
+			title: "Logical Address | Register your Logical Address",
+			page: 'register',
+			app_title: "Logical Address"
+		});
+	});
+	
+	app.get('/register/business',function (req, res, next) {
+		res.render('pages/register-business', {
+			title: "Logical Address | Logical Address for Business",
+			page: 'register-business',
+			app_title: "Logical Address"
+		});
 	});
 };
