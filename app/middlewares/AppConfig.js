c4fc3f35b5cca062d727143ec533ee8dbdb67d04
DR <d.retnan@nhubnigeria.com>
2017-04-26 20:45:09 +0000

    middleware loaded for all route, export appconfig variables to ejs

---
+++ app/middlewares/AppConfig.js
@@ -0,0 +1,8 @@
+var AppConfig = require("../../config/config");
+module.exports = function (app) {
+    app.all('*', function(req, res, next){
+        res.locals.AppConfig = AppConfig;
+        res.locals.ENV = process.env.NODE_ENV;
+        next();
+    });
+};
